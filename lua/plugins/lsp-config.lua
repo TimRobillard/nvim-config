c91b7c4d68742b373a93ffd3294296b6e3cd169c
Tim Robillard <robillt@algonquincollege.com>
2024-02-27 14:51:37 -0500

    update lsps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,39 +1,52 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilites = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilites = capabilites,
-			})
-			lspconfig.tsserver.setup({
-				capabilites = capabilites,
-			})
-			lspconfig.html.setup({
-				capabilites = capabilites,
-			})
-      lspconfig.tailwindcss.setup({
-        capabilites = capabilites
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "gopls", "html", "htmx", "tailwindcss" },
       })
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "templ" },
+      })
+      lspconfig.htmx.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "templ" },
+      })
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+        filetypes = { "templ", "javascript", "typescript", "react" },
+        init_options = { userLanguages = { templ = "html" } },
+      })
+      lspconfig.templ.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,4 +5,5 @@ vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
 vim.cmd("set title")
 vim.cmd("set titlestring=%f")
+vim.filetype.add({ extension = { templ = "templ" } })
 vim.g.mapleader = " "
