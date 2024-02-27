35d35d95d5928811bd20e4fd89f48bf2148d8cc1
Tim Robillard <tim@brokerbay.com>
2024-02-27 14:49:09 -0500

    add tab titles

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,5 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.cmd("set title")
+vim.cmd("set titlestring=%f")
 vim.g.mapleader = " "
-
